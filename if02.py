c09b3199f1fe0a1ec0b4a1ddf3e9b852b7bba270
Mamaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2022-10-28 22:16:53 +0500

    2-m

--- if02.py
+++ if02.py
@@ -8,4 +8,10 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    if a<c and a<b and c<b:
+        return a
+    if a>c and b>c and a>b:
+        return c
+    if a>b and c>b and a>c:
+        return b
+print (main(1,4,2))
